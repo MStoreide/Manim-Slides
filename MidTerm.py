59968fd36a10b450d70b58314ea763e00d14f272
Markus <markus.storeide@gmail.com>
2023-04-25 11:17:10 +0200

    Notes

---
+++ MidTerm.py
@@ -0,0 +1,45 @@
+from manim import *
+from manim_slides import Slide, ThreeDSlide
+
+#Should also have a packup Powerpoint just in case
+
+### Content ###
+
+# Intro 
+# Lab Engineer Position (Drones etc)
+# Course Progress
+# Outreach Participation
+# Secondments and Travel
+# Timeplan
+
+
+
+## Courses ##
+
+
+## Travel, Outreach and Participation ##
+
+# All NO-CHANGE visits and presentations
+# MANER Mobility at Yale
+# MANER Training School
+# CHANGE Training School
+
+# Conferences
+
+# DT4BH in Orleans
+# Archiving 2023
+
+
+## Research ##
+
+# 3D Scanning - Point Clouds
+# Simplification Algorithms - Visualize Examples
+# Acquisition of Color and Texture
+# Fusing Color and Texture
+
+
+## Publications ##
+
+
+
+## Timeplan ##
