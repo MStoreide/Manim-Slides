308e6d7288fb4d29fe53b18866fd9a40795762e5
Markus <markus.storeide@gmail.com>
2023-04-25 14:55:33 +0200

    MidTerm Draft

--- MidTerm.py
+++ MidTerm.py
@@ -3,6 +3,28 @@ from manim_slides import Slide, ThreeDSlide
 
 #Should also have a packup Powerpoint just in case
 
+class Midterm(ThreeDSlide):
+    def construct(self):
+        self.camera.background_color = GRAY_E
+
+#Slide 1: Cover
+
+        colorlab = ImageMobject("/home/markus/Priv_Manim_Slides/Manim-Slides/Logos/CLsmall.png").move_to([3,0.5,0])
+        colorlab.scale(2)
+        colorlabtext = Text("Colourlab").move_to([-1,0,0])
+        colorlabtext.scale(2)
+        cg = Text("IDIG4002 - Computer Graphics").move_to([0,-1,0])
+        cg.scale(0.5)
+        colorlabcorner = ImageMobject("/home/markus/Priv_Manim_Slides/Manim-Slides/Logos/CLsmall.png/").to_corner(DOWN + RIGHT)
+        colorlabcorner.scale(0.5)
+        NTNU = ImageMobject(f"/home/markus/Priv_Manim_Slides/Manim-Slides/Logos/NTNU.png").move_to([0, 2, 0])
+        NTNU.scale(0.3)
+        NTNUcorner = ImageMobject(f"/home/markus/Priv_Manim_Slides/Manim-Slides/Logos/NTNU.png").move_to([-5, -3, 0])
+        NTNUcorner.scale(0.2)
+
+        self.play(FadeIn(colorlab, shift=RIGHT*2), FadeIn(NTNU, shift=RIGHT*2), Write(colorlabtext, shift=LEFT*2), Write(cg, run_time=0.7))
+        self.next_slide()
+
 ### Content ###
 
 # Intro 
@@ -65,3 +87,10 @@ quadric_error_eq = Matrix([q_11,q_12,q_13,q_14],
                           [q_12,q_22,q_23,q_24],
                           [q_13,q_23,q_33,q_34],
                           [0,0,0,1]^-1)
+
+color_rectangle = Rectangle(
+                            width=FRAME_WIDTH-1,
+                            height=1,
+                            fill_opacity=1,
+                            sheen_direction=RIGHT,
+                            stroke_width=0,)
