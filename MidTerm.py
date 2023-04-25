853917fcf908f7b719f63f399a65fb3989be6120
Markus <markus.storeide@gmail.com>
2023-04-25 13:29:33 +0200

    Quadric Error

--- MidTerm.py
+++ MidTerm.py
@@ -60,3 +60,8 @@ chamfer_eq = MathTex(r"d_CD (X,Y) = \sum_{x \in X}min(MISSING) \| x-y \| 2² + \
 
 rendering_eq = MathTex(r"L_o(x, \omega_o, \Lambda, t) = L_e(x, \omega_o, \Lambda, t) + \int_{\Omega} f_r(x, \omega_i, \omega_o, \Lambda, t)(\omega_i . \textbf{n}) d \omega_i")
 # Does not account for: Transmission, Subsurface Scattering, Polarization, Phosphorescence, Interference, Flourescence.
+
+quadric_error_eq = Matrix([q_11,q_12,q_13,q_14],
+                          [q_12,q_22,q_23,q_24],
+                          [q_13,q_23,q_33,q_34],
+                          [0,0,0,1]^-1)
