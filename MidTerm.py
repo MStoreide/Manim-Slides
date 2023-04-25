7b2e29e3f06592986ffd062eaf0b3ee9fa72ce87
Markus <markus.storeide@gmail.com>
2023-04-25 13:24:33 +0200

    Chamfer Equation

--- MidTerm.py
+++ MidTerm.py
@@ -56,4 +56,7 @@ from manim_slides import Slide, ThreeDSlide
 
 hausdorff_eq = MathTex(r"d_h(X,Y) = max(sup inf d(x,y), sup inf d(x,y))")
 
+chamfer_eq = MathTex(r"d_CD (X,Y) = \sum_{x \in X}min(MISSING) \| x-y \| 2² + \sum_{y \in Y} min(MISSING) \| x-y \| 2²")
+
 rendering_eq = MathTex(r"L_o(x, \omega_o, \Lambda, t) = L_e(x, \omega_o, \Lambda, t) + \int_{\Omega} f_r(x, \omega_i, \omega_o, \Lambda, t)(\omega_i . \textbf{n}) d \omega_i")
+# Does not account for: Transmission, Subsurface Scattering, Polarization, Phosphorescence, Interference, Flourescence.
