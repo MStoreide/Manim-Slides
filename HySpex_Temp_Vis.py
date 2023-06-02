3321479bef63f8ad1a0897bfbe676cec0faaa821
Markus <markus.storeide@gmail.com>
2023-06-02 11:12:53 +0200

    Comments

--- HySpex_Temp_Vis.py
+++ HySpex_Temp_Vis.py
@@ -1,9 +1,18 @@
 import pandas as pd
 import numpy as np
 
+# You must be logged in to the Colorlab server to use the script!
+
+# Add lines that loads all the temp files in the folder
+# 
+
 temps = np.loadtxt(f"/home/markus/Temperature_Log_AIM384_2023-03-12T153147.txt",
                     skiprows = (2))
 
 HS = pd.DataFrame(temps, columns = ["Time(s)", "Sensor(K)", "Ready"])
 print(HS)
+
+# Plot them against each other
 HS.plot()
+
+# Impleent so that it prints the date of the measurement. Maybe as the DF name?
