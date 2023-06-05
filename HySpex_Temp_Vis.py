fd867b608977feb3eb07295238eac373ac8324a8
Markus <markus.storeide@gmail.com>
2023-06-05 11:21:17 +0200

    ArchivingPresentation

--- HySpex_Temp_Vis.py
+++ HySpex_Temp_Vis.py
@@ -8,7 +8,6 @@ import numpy as np
 
 temps = np.loadtxt(f"/home/markus/Temperature_Log_AIM384_2023-03-12T153147.txt",
                     skiprows = (2))
-
 HS = pd.DataFrame(temps, columns = ["Time(s)", "Sensor(K)", "Ready"])
 print(HS)
 
